3813efd389ff3054e0a2d41ec3a007f7f39a3fc5
JavierLomasH <javierlomash@gmail.com>
2025-04-08 14:39:28 -0500

    Update new_program.c

--- new_program.c
+++ new_program.c
@@ -13,20 +13,34 @@ int main (int argc, char *argv[]) {
         scanf("%s", &materias[cont]);
         cont++;
     }    
-
+    cont=0;
     do{
         printf("___________________________\n");
         printf("Seleccione opcion:\n");
         printf("1. Registrar estudiante y notas\n");
         printf("2. Cambiar notas de estudiante\n");
         printf("3. Promedio por materia/estudiante\n");
-        printf("4. Calificaciónes altas y bajas\n");
+        printf("4. Calificaciones altas y bajas\n");
         printf("5. Aprobados y reprobados por materia\n");
         printf("6. Salir\n");
         printf("___________________________\n");
         printf(">> ");
         scanf("%d",&opc);
+        switch (opc){
+            case 1:        
+                printf("Ingrese el nombre de la estudiante %d: ", cont+1);
+                scanf("%s", &nombres[cont]);
+                for (int i = 0; i < 3; i++){
+                    printf("Ingrese calificacion de %s: ", materias[i]);
+                    scanf("%s", &notas[cont][i]);
+                }
+                cont++;
+                break;
+                    
+            default:
+                break;
+            }
 
     }while(opc!=6);
+
     return 0;
-}
